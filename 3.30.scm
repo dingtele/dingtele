22cd075885d08f4c4214da7d64b2404e904c6a86
dingtele <sardine323@gmail.com>
2023-10-12 18:17:03 +0800

    11

--- 3.30.scm
+++ 3.30.scm
@@ -1,13 +1,1 @@
-(define (ripple-carry-adder a-list b-list c s-list)
-    (let ((c-in-list (map (lambda(x)(make-wire)) a))))
-        (map full-adder a-list 
-                        b-list 
-                        c-in-list
-                        s-list
-                        (cons c c-in-list)))
-
-(define (ripple-carry-adder-2 a-list b-list c-in s-list)
-    (define (iterator a-list b-list c s-list c-out)
-        (full-adder (car a) (car d) c (car s) c-out)
-           (iterator (cdr a-list) (cdr b-list) c-out (cdr s) (make-wire)))
-    (iterator a-list b-list c-in s-list (make-wire)))
+(define (ripple-carry-adder))
